1584fa9be7857e88466030c73abbb9c506480fb0
Thanh Binh <ttbinh@hueic.edu.vn>
2019-09-03 11:02:04 +0700

    add propper.js

--- header.php
+++ header.php
@@ -20,6 +20,9 @@
 
 	 <script src="https://use.fontawesome.com/9c12a2c8b5.js"></script>
 	<?php wp_head(); ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
 <body <?php body_class("site com-sppagebuilder view-page no-layout no-task itemid-437 vi-vn ltr  sticky-header layout-fluid off-canvas-menu-init"); ?>>
